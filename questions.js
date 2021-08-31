e15985c628a55ea25b2f187a3c3ea9e1c4542a73
Blu VIII <34254871+BluVolpe@users.noreply.github.com>
2021-08-31 19:39:01 -0400

    Removed folders & updated sylesheet

---
+++ questions.js
@@ -0,0 +1,67 @@
+let questions = [
+  {
+    question: "What does HTML stand for?",
+    choice: [
+      "Hyper Trainer Marking Language",
+      "Hyper Text Marketing Language",
+      "Hyper Text Markup Language",
+      "Hyper Text Markup Leveler",
+    ],
+    answer: "Hyper Text Markup Language",
+  },
+  {
+    question: "<h1>Text</h1> is the correct way of making a header in HTML.",
+    choice: ["True", "False"],
+    answer: "True",
+  },
+  {
+    question: "The Tag is used To Give Heading To The Table.",
+    choice: ["Head", "Td", "Th", "Caption"],
+    answer: "Head",
+  },
+  {
+    question: "What type of function is used to parse a string to Int:?",
+    choice: ["Integer.Parse", "Int.Parse", "Parse.Int", "None"],
+    answer: "Parse.Int",
+  },
+  {
+    question: "A Function Associated With An object is Called:",
+    choice: ["Function", "Method", "Link", "None"],
+    answer: "Method",
+  },
+  {
+    question: "If button is clicked... event handler is invoked.",
+    choice: ["OnSubmit()", "OnLoad()", "IsPostBack()", "OnClick()"],
+    answer: "OnClick()",
+  },
+  {
+    question: "IsNaN() Evaluates And Argument To Determine if Given Value:",
+    choice: [
+      "Is not a null",
+      "Is not a number",
+      "Is not a new object",
+      "None of the above",
+    ],
+    answer: "Is not a number",
+  },
+  {
+    question: "JavaScript File has an extension of:",
+    choice: [".Java", ".Js", ".javascript", ".xml"],
+    answer: ".Js",
+  },
+  {
+    question: "Event used to check an empty text box:",
+    choice: ["OnClick()", "OnFocus()", "OnBlur()", "None"],
+    answer: "OnBlur()",
+  },
+  {
+    question: "GetMonth() returns the month as:",
+    choice: ["Int", "Float", "Char", "String"],
+    answer: "Int",
+  },
+  {
+    question: "Method Prompt() contain ___ number of parameters",
+    choice: ["One", "Two", "Three", "Zero"],
+    answer: "Two",
+  },
+];
